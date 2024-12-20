677b2c8dc16d4ae199003443c4595a88e0d80af0
Mark Eaton <MarkEEaton@users.noreply.github.com>
2024-12-20 12:58:15 -0500

    Update auth workflow

--- toot.py
+++ toot.py
@@ -1,12 +1,22 @@
+import os
 import random
 from mastodon import Mastodon
 
+base_url = 'https://mastodon.ocert.at'
 
-# Create actual API instance
-mastodon = Mastodon(
-    access_token = 'mahabharabot_usercred.secret',
-    api_base_url = 'https://mastodon.ocert.at'
+app = Mastodon.create_app(
+    'pytooterapp',
+    api_base_url = base_url,
 )
+
+mastodon = Mastodon(client_id=app[0], client_secret=app[1], api_base_url=base_url)
+token = mastodon.log_in(
+    os.environ['MAHA_EMAIL'],
+    os.environ['MAHA_PWD'],
+)
+
+mastodon = Mastodon(access_token=token, api_base_url=base_url)
+
 
 with open('O.txt', 'r') as infile:
     toots = infile.readlines()
